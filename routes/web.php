78390f948844ffb47ea649ec507369f51d3621bf
Abejas242 <55117180+Abejas242@users.noreply.github.com>
2019-11-07 11:42:39 -0500

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,6 @@ Route::get('/', function () {
 
     return view('welcome');
 });
-
-Route::get('miAPP', function () {
-    return 'Esta es mi APP';
-});
-
-
 Route::get('MiApp', function () {
 
     return 'Esta es mi aplicación';
@@ -33,29 +27,14 @@ Route::get('/estimates', 'EstimateController@index');
 Route::get('/analysis', 'AnalysisController@index');
 Route::get('/help', 'HelpController@index');
 Route::get('/home', 'HomeController@index');
-Route::get('/home', 'HomeController@index');
 
 Route::resource('controler', 'Controller');
 
-Route::post('/statistics', 'StatisticsController@store');
 Route::post('/statistics', 'StatisticsController@store');
 Route::post('/Analysis', 'AnalysisController@store');
 
 Route::name('consultar')->get('/consultar','Controller@consultar');
 Route::name('estimar')->get('/estimar','Controller@estimar');
 Route::name('imprimir')->get('/imprimir', 'Controller@imprimir');
-
-
-Route::post('/statistics', 'StatisticsController@store');
-Route::post('/Analysis', 'AnalysisController@store');
-
-Auth::routes();
-
-
-
-
-
-
->>>>>>> 193f0f8334b0aa94501f10fe7c7eb1ac923aa3c1
 
 Auth::routes();
